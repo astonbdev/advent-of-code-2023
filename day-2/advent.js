c7c624f0844f3d584f03569150f9b5f9a2ac19c6
Brian Aston <astonb.dev@gmail.com>
2023-12-03 15:31:26 -0800

    Solved part 1

---
+++ day-2/advent.js
@@ -0,0 +1,83 @@
+const token = process.env.AOC_24_TOKEN;
+
+/**
+ * fetch the puzzle data and return the response
+ * @returns puzzle text
+ */
+async function getPuzzleInput() {
+  const response = await fetch("https://adventofcode.com/2023/day/2/input", {
+    headers: {
+      cookie: token,
+    },
+  });
+  const text = await response.text();
+
+  return text;
+}
+
+function getGameFreqs(input) {
+  const MAX_COLORS = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  let games = input.split("\n");
+  games = games.map((game) => game.split(";"));
+
+  const regex = /\d+ red|\d+ green|\d+ blue/g;
+  let gameStats = [];
+
+  games = games.map((game) => {
+    gameStats = game.map((game) => {
+      return game.match(regex);
+    });
+    return gameStats;
+  });
+
+  let total = 0;
+  for (let i = 0; i < games.length - 1; i++) {
+    let valid_game = true;
+    for (const pulls of games[i]) {
+      for (const pull of pulls) {
+        // console.log("🚀 ~ file: advent.js:42 ~ getGameFreqs ~ pull:", pull);
+
+        const [number, color] = pull.split(" ");
+        // console.log("🚀 ~ file: advent.js:44 ~ getGameFreqs ~ test:", test);
+
+        // console.log("🚀 ~ file: advent.js:43 ~ getGameFreqs ~ color:", color);
+        // console.log("🚀 ~ file: advent.js:43 ~ getGameFreqs ~ number:", number);
+        // console.log(
+        //   "🚀 ~ file: advent.js:46 ~ getGameFreqs ~ MAX_COLORS:",
+        //   MAX_COLORS[color]
+        // );
+        if (number > MAX_COLORS[color]) valid_game = false;
+      }
+    }
+
+    if (valid_game) {
+      total += i + 1;
+    }
+  }
+
+  console.log("🚀 ~ file: advent.js:54 ~ getGameFreqs ~ total:", total);
+  return total;
+  //   const gameStats = bagPulls.map((p) => {});
+}
+
+const MAX_VALUES = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+async function testGames(testInput) {
+  const input = testInput ? testInput : await getPuzzleInput();
+
+  gameCounters = getGameFreqs(input);
+}
+
+const testInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+testGames().then((answer) => console.log("PUZZLE SOLUTION:", answer));
